54bd5d95ed04daae72d8c52c067a9a3e212b5e85
JHeadland <jth9@hawaii.edu>
2016-11-12 19:12:53 -1000

    Added pygame demo 11/12/16

---
+++ LinuxPython/PygameDemo-11-12-16/demopygame.py
@@ -0,0 +1,28 @@
+import sys, pygame
+
+pygame.init()
+
+screenX = 2000
+screenY = 1000
+
+screen = pygame.display.set_mode((screenX, screenY))
+pygame.display.set_caption('My Game')
+
+gilgamesh = pygame.image.load('gilg.jpg').convert()
+gilgRect = gilgamesh.get_rect()
+
+#gilgRect.centerx = 200
+
+while True:
+	
+	screen.fill((255, 255, 255))
+	screen.blit(gilgamesh, gilgRect)
+
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			sys.exit()
+		elif event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_RIGHT:
+				gilgRect.x += 10
+
+	pygame.display.flip()
